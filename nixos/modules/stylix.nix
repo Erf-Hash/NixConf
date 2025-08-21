64e33c5013a1b424e35d6e1596a774ecd7cd245c
erf <erf-hash@proton.me>
2025-08-21 15:23:19 +0000

    Changed wallpaper

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -4,10 +4,11 @@
         enable = true;
         polarity = "dark";
         base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml"; 
-        image = pkgs.fetchurl {
-            url = "https://www.pixelstalk.net/wp-content/uploads/2025/04/Bustling-evening-atmosphere-in-a-Tokyo-Cityscape-Wallpaper-composition.webp";
-            hash = "sha256-4I7cRDS/seJkWGbe1jy79WewjTifx9gnp/slkcMM+ec=";
-        };
+        image = ./wallpaper.jpg;
+       # image = pkgs.fetchurl {
+       #     url = "https://wallpaperaccess.com/download/tokyo-44174";
+       #     hash = "sha256-4I7cRDS/seJkWGbe1jy79WewjTifx9gnp/slkcMM+ec=";
+       # };
 
         fonts = { 
             serif = { package = pkgs.dejavu_fonts; name = "DejaVu Serif"; };
